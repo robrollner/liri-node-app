3974afe586eac856d043b80a0d5be5284b7c9369
Robert Rollner <robrollner@gmail.com>
2017-09-20 20:52:18 -0600

    adding keys,js raw file

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded, but I am not');
+
+var twitterKeys = {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+}
+
+module.exports = twitterKeys;
