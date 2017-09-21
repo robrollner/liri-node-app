9b2ca7d23ae9249cb120ea747b4d5d46f05077d7
Robert Rollner <robrollner@gmail.com>
2017-09-20 21:01:35 -0600

    added keys to keys.js file

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded, but I am not');
 
 var twitterKeys = {
-    consumer_key: '',
-    consumer_secret: '',
-    access_token_key: '',
-    access_token_secret: ''
+    consumer_key: 'rDFQplxfP7LabPU3a6ajpqSeE',
+    consumer_secret: 'O63M9qsaX9fM0zswpPXWfgnE98hfU384Wk8xRbR42y1Sfl5Pcd',
+    access_token_key: '578585533-AKQxZqmeCioWz0DDVklZcwKhf1O8xbaHgf9g2Cnv',
+    access_token_secret: 'h4vhid8nAE3gdXNQgZZ3m0vYsgj6wtQ95odhOqe0815mS'
 }
 
 module.exports = twitterKeys;
