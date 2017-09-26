3f3ece0d884e2fc98d81fe898f71c1d87510a61d
Robert Rollner <robrollner@gmail.com>
2017-09-25 20:18:50 -0600

    working app, only loads 1 tweet but might be due to timing

--- liri.js
+++ liri.js
@@ -19,35 +19,30 @@ function nodeApp() {
             'do-what-it-says' // No you are!
         ]
     }]).then((results) => {
-        switch (results.program) {
-            case 'my-tweets':
-                tweeter();
-                break;
 
-            case 'spotify-this-song':
-                inquirer.prompt([{
-                    type: "input",
-                    name: "song",
-                    message: "Name a song!",
-                }]).then((results) => {
-                    let song = results.song;
-                    songInfo(song);
-                });
-                break;
-
-            case "movie-this":
-                inquirer.prompt([{
-                    type: "input",
-                    name: "movie",
-                    message: "Name a movie!",
-                }]).then((results) => {
-                    let movie = results.movie;
-                    movieInfo(movie);
-                });
-                break;
-
-                // case "do-what-it-says":
-                // console.log("")
+        if (results.program === "my-tweets") {
+            tweeter();
+        } else if (results.program === "spotify-this-song") {
+            inquirer.prompt([{
+                type: "input",
+                name: "song",
+                message: "Name a song!",
+            }]).then((results) => {
+                let song = results.song;
+                songInfo(song);
+            });
+        } else if (results.program === "movie-this") {
+            inquirer.prompt([{
+                type: "input",
+                name: "movie",
+                message: "Name a movie!",
+            }]).then((results) => {
+                let movie = results.movie;
+                movieInfo(movie);
+            });
+        } else if (results.program === "do-what-it-says") {
+            nodeApp();
+            //do something but not sure what...
         }
     })
 }
@@ -64,11 +59,15 @@ function tweeter() {
 
         console.log("\nHere's what I've had to say! ");
         for (var i = 0; i < tweets.statuses.length; i++) {
-            console.log(tweets.statuses[i]);
+            console.log(tweets.statuses[i].created_at.substring(0, 19) + tweets.statuses[i].text);
         }
         console.log('');
     })
+    setTimeout(reStart, 1500);
+
 }
+
+
 
 function songInfo(song) {
     let Spotify = new spotify(keys.spotifyKeys);
@@ -91,4 +90,55 @@ function songInfo(song) {
             console.log(`Click here to listen! ${songURL}\n`);
 
         })
+    setTimeout(reStart, 1500);
+
+}
+
+
+
+function movieInfo(movie) {
+    // let apiKey = "40e9cece";
+    let movieUrl = "http://www.omdbapi.com/?t=" + movie + "&apiKey=40e9cece";
+
+    request(movieUrl, (error, response, body) => {
+        if (JSON.parse(body).response === "false") {
+            console.log("\nNo movie for you!");
+        } else if (!error && response.statusCode === 200) {
+            let title = JSON.parse(body).Title;
+            let year = JSON.parse(body).Year;
+            let origin = JSON.parse(body).Country;
+            let language = JSON.parse(body).Language;
+            let plot = JSON.parse(body).Plot;
+            let actors = JSON.parse(body).Actors;
+            let IMDB = JSON.parse(body).imdbRating;
+            let rottenTomato = JSON.parse(body).Ratings[1];
+
+            console.log(title);
+            console.log(year);
+            console.log(origin);
+            console.log(language);
+            console.log(plot);
+            console.log(actors);
+            console.log(IMDB);
+            console.log(rottenTomato);
+        }
+    })
+    setTimeout(reStart, 1500);
+
+}
+
+function reStart() {
+    inquirer.prompt([{
+        type: "checkbox",
+        name: "confirm",
+        message: "Do you want to try again?",
+        choices: ["Yes", "No"]
+    }]).then((results) => {
+        if (results.choices === "Yes") {
+            nodeApp();
+        } else {
+            console.log("\nYou are no fun!");
+        }
+    })
+
 }
